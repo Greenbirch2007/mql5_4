2894fe3f602da5f99a861ec1211dc40e5ea6331a
Greenbirch2007 <min2020fr@gmail.com>
2022-03-08 22:52:46 +0800

    first commit

---
+++ MQL & python/1. 5分钟超80点，通知微信/test.py
@@ -0,0 +1,16 @@
+
+
+
+import os
+
+base_file=''
+
+# file_handle=open('put.txt',mode='w')
+#
+# print(os.path.exists("put.txt"))
+# print(os.path.exists("call.txt"))
+#
+# if
+
+
+print(os.path.exists("call.txt") is False)
